6140b9be832e2da6b2c70f3de699b30151e109ea
martin.wang <357951@qq.com>
2021-11-15 18:11:09 +0800

    a

--- function.py
+++ function.py
@@ -8,6 +8,8 @@ from random import choice
 from itchat.content import *
 KEY = '983eb78b67a046cd9b2f8c58b9751d8a'
 from yuliao import a
+from city_code2 import city_code2
+import requests
 # db_host = "47.91.228.122"
 # db_name = "bot"
 # db_user = "root"
@@ -53,30 +55,97 @@ from yuliao import a
 
 @itchat.msg_register(TEXT)
 def text_reply(msg):
+    if msg['Content'] == 'help':
+        data = '\n回复:help  展示命令帮助\n' \
+               '回复:已有指令 可以进行对答\n' \
+               '查询天气：msg=天气深圳\n' \
+               '查询中英翻译：msg=翻译i love you\n' \
+               '查询智能聊天：msg=你好\n' \
+               '查询笑话：msg=笑话\n' \
+               '查询歌词⑴：msg=歌词后来\n' \
+               '查询歌词⑵：msg=歌词后来-刘若英\n' \
+               '查询计算⑴：msg=计算1+1*2/3-4\n' \
+               '查询计算⑵：msg=1+1*2/3-4\n' \
+               '查询ＩＰ⑴：msg=归属127.0.0.1\n' \
+               '查询ＩＰ⑵(暂时不支持)：msg=127.0.0.1\n' \
+               '查询手机⑴：msg=归属13430108888\n' \
+               '查询手机⑵：msg=13430108888\n' \
+               '查询成语查询：msg=成语一生一世\n' \
+               '查询五笔/拼音：msg=好字的五笔/拼音\n' \
+               ''
+        # '回复:xx市天气或者xx市xx区天气或者xx(市)天气  会回复该市当天天气\n' \
+        # '例如: 北京市天气,北京天气,北京市朝阳区天气\n' \
+        # '回复:XX是猪 回复你说的对，%s真的是猪^(*￣(oo)￣)^' \
+        return u"[我是机器人小渣渣：]{}".format('%s' % data)
     try:
         aa = choice(a[msg['Content']])
         return aa
     except:
+        msgs = qingyunkeApi(msg)
+        return msgs
         # 当消息不是由自己发出的时候
-        return u"[我是渣男的机器人小渣渣：正在测试自动机器人]{}".format('.')
+        # return u"[我是机器人小渣渣：正在测试机器人]{}".format('.')
         # 回复给好友
 
 
 @itchat.msg_register('Text',isGroupChat=True)
 def text_reply(msg):
-    if msg['Content'][-2:] =='是猪':
-        print(msg['Content'])
-        # 当消息不是由自己发出的时候
-        return u"[我是机器人小渣渣：]{}".format('你说的对，%s真的是猪^(*￣(oo)￣)^' % msg['Content'][:-2])
-    elif msg['Content']=='help':
-        data='回复:help  展示命令帮助' \
-             '回复:已有指令 可以进行对答' \
+    msg = msg['Content']
+    # 当回复是XXXX是猪时会回复信息.趣味.
+    if msg[-2:] =='是猪':
+        return u"[我是机器人小渣渣：]{}".format('你说的对，%s真的是猪^(*￣(oo)￣)^' % msg[:-2])
+    # 如果结尾是天气的会根据规则截取并返回天气信息
+    elif msg[-2:] =='天气':
+        msg = msg[0:-2]
+        try:
+            city = msg.split('市')[0]
+            qu = city
+        except:
+            city = msg.split('天气')[0]
+            qu = city
+        try:
+            qu = msg.split('市')[1].split('区')[0]
+            if qu =='':
+                qu = city
+        except:
+            qu = city
+        print(city + qu)
+        try:
+            msg= get_weather(city, qu)
+        except:
+            msg = "回复:xx市天气或者xx市xx区天气或者xx(市)天气  会回复该市当天天气\n"
+        return msg
+    # 增加青云客智能Api
+    elif msg[:2]=='天气' or msg[:2] =='翻译' or msg[:2] =='笑话' or msg[:2] =='歌词' or msg[:2] =='计算' or msg[:2] =='归属'or msg[:2] =='成语' or msg[-5:] =='五笔/拼音':
+        msgs = qingyunkeApi(msg)
+        return msgs
+    # 增加帮助指令.涵盖可以使用的命令
+    elif msg=='help':
+
+        data='\n回复:help  展示命令帮助\n' \
+             '回复:已有指令 可以进行对答\n' \
+             '回复:xx市天气或者xx市xx区天气或者xx(市)天气  会回复该市当天天气\n' \
+             '例如: 北京市天气,北京天气,北京市朝阳区天气\n' \
              '回复:XX是猪 回复你说的对，%s真的是猪^(*￣(oo)￣)^' \
+             '查询天气：msg=天气深圳\n' \
+             '查询中英翻译：msg=翻译i love you\n' \
+             '查询智能聊天(暂时不支持)：msg=你好\n' \
+             '查询笑话：msg=笑话\n' \
+             '查询歌词⑴：msg=歌词后来\n' \
+             '查询歌词⑵：msg=歌词后来-刘若英\n' \
+             '查询计算⑴：msg=计算1+1*2/3-4\n' \
+             '查询计算⑵(暂时不支持)：msg=1+1*2/3-4\n' \
+             '查询ＩＰ⑴：msg=归属127.0.0.1\n' \
+             '查询ＩＰ⑵(暂时不支持)：msg=127.0.0.1\n' \
+             '查询手机⑴：msg=归属13430108888\n' \
+             '查询手机⑵(暂时不支持)：msg=13430108888\n' \
+             '查询成语查询：msg=成语一生一世\n' \
+             '查询五笔/拼音：msg=好字的五笔/拼音\n' \
              ''
         return u"[我是机器人小渣渣：]{}".format('%s' % data)
     else:
         try:
-            aa = choice(a[msg['Content']])
+            aa = choice(a[msg])
             return aa
         except:
             # 当消息不是由自己发出的时候
@@ -84,7 +153,36 @@ def text_reply(msg):
             # 回复给好友
             pass
 
-# if __name__ == '__main__':
-#     itchat.auto_login(enableCmdQR=2)#enablecmdqr参数是用于在命令行上生成二维码，用于linux服务器
-itchat.auto_login(enableCmdQR=2,hotReload=True)
-itchat.run(debug=True)
+
+def qingyunkeApi(msg):
+    url = 'http://api.qingyunke.com/api.php?key=free&appid=0&msg=%s'
+    aaa = requests.get(url % msg)
+    print(aaa.json())
+    msgs = aaa.json()['content'].replace('{br}','\n')
+    return msgs
+
+
+def get_weather(city,qu):
+    # 天气接口获取当天的天气信息
+    url = 'http://t.weather.itboy.net/api/weather/city/%s'
+    #获取地区code
+    aa = city_code2[city][qu]['city_code']
+    print(url % aa)
+    aaa = requests.get(url % aa)
+    msgs = aaa.json()
+    msg = '本次数据更新时间为:' + msgs['cityInfo']['updateTime'] + '\n' + \
+          ' 今天:' + msgs['data']['forecast'][0]['fx'] + msgs['data']['forecast'][0]['fl'] + msgs['data']['forecast'][0][
+              'type'] + '\n' + \
+          ' 今天最高气温:' + msgs['data']['forecast'][0]['high'] + '\n' + \
+          ' 今天最低气温:' + msgs['data']['forecast'][0]['low'] + '\n' + \
+          ' 日出时间为:' + msgs['data']['forecast'][0]['sunrise'] + '\n' + \
+          ' 日落时间为:' + msgs['data']['forecast'][0]['sunset'] + '\n' + \
+          ' PS:' + msgs['data']['forecast'][0]['notice'] + '\n'
+    return msg
+
+if __name__ == '__main__':
+    #     #     itchat.auto_login(enableCmdQR=2)#enablecmdqr参数是用于在命令行上生成二维码，用于linux服务器
+    #     itchat.auto_login(enableCmdQR=2)
+    #     itchat.run(debug=True)
+    itchat.auto_login(enableCmdQR=False)
+    itchat.run(debug=True)
